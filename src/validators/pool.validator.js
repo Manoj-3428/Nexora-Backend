23c53ad7d3e9721e0a1b7323800c5a8561faebf5
Manoj Kommana <kommana.manoj.in@rupeek.com>
2026-05-10 17:33:18 +0530

    Updated BackendCode

--- src/validators/pool.validator.js
+++ src/validators/pool.validator.js
@@ -7,18 +7,18 @@ const createPoolSchema = Joi.object({
   expiresAt: Joi.date().iso().optional(),
   isPublic: Joi.boolean().optional(),
   passwordProtected: Joi.boolean().optional(),
-  password: Joi.string().min(4).when('passwordProtected', { is: true, then: Joi.required() }),
-  localIp: Joi.string().ip().optional(),
-  port: Joi.number().port().optional(),
+  password: Joi.string().min(4).allow(null, '').optional().when('passwordProtected', { is: true, then: Joi.required() }),
+  localIp: Joi.string().ip().allow(null, '').optional(),
+  port: Joi.number().port().allow(null).optional(),
   protocolType: Joi.string().valid(...Object.values(PoolProtocol)).optional(),
-});
+}).unknown(true); // Allow unknown fields for forward compatibility in coordination layer
 
 const updatePoolSchema = Joi.object({
   poolName: Joi.string().min(3).max(100).optional(),
   isPublic: Joi.boolean().optional(),
   passwordProtected: Joi.boolean().optional(),
-  password: Joi.string().min(4).optional(),
-});
+  password: Joi.string().min(4).allow(null, '').optional(),
+}).unknown(true);
 
 module.exports = {
   createPoolSchema,
